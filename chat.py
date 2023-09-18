e249db2fe23c67884b0409c1751efbfc9b3ee550
Konsta Martikainen <konsta.martikainen@gmail.com>
2023-09-18 15:11:21 +0300

    Message generation in different file

---
+++ chat.py
@@ -0,0 +1,17 @@
+import random
+async def message_generation(message):
+    brooklyn_99_quotes = [
+            'I\'m the human form of the 💯 emoji.',
+            'Bingpot!',
+            (
+                'Cool. Cool cool cool cool cool cool cool, '
+                'no doubt no doubt no doubt no doubt.'
+            ),
+            'qväck kväck! - swedish ducks',
+        ]
+ 
+    # Check if the message contains the phrase "beep boop"
+    if 'beep boop' not in message.content.lower():
+        # If not, respond with quote
+        response = random.choice(brooklyn_99_quotes)
+        return response
--- handler.py
+++ handler.py
@@ -1,23 +1,6 @@
 import random
+from chat import message_generation
 
 async def handle_message(message):
-
-    brooklyn_99_quotes = [
-            'I\'m the human form of the 💯 emoji.',
-            'Bingpot!',
-            (
-                'Cool. Cool cool cool cool cool cool cool, '
-                'no doubt no doubt no doubt no doubt.'
-            ),
-            ('qväck kväck! - swedish ducks'),
-        ]
-
-    if message.content == '99!':
-        response = random.choice(brooklyn_99_quotes)
-        await message.channel.send(response)
-        
-    # Check if the message contains the phrase "beep boop"
-    if 'beep boop' not in message.content.lower():
-        # If not, respond with quote
-        response = random.choice(brooklyn_99_quotes)
-        await message.channel.send(response)
+    response = message_generation(message)
+    await message.channel.send(response)
